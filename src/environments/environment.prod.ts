d328e5ab4d4a218dfb6361ef5253de68f501601e
Nicola Toledo <hello@nicolatoledo.dev>
2020-05-08 17:01:08 +0200

    modifica testo piu grande

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCXLbcLz5l_DTWJ9TifitDM0v6XYNwLCSs",
+    authDomain: "corso-angular-pro.firebaseapp.com",
+    databaseURL: "https://corso-angular-pro.firebaseio.com",
+    projectId: "corso-angular-pro",
+    storageBucket: "corso-angular-pro.appspot.com",
+    messagingSenderId: "1014951085541",
+    appId: "1:1014951085541:web:73afbbcfeb778c2759db0c",
+    measurementId: "G-6W7H7ZYB14",
+  },
+  moviedb: {
+    access_token:
+      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMWNmNTg0ZjczZWEzMWE0ZjA5ZWEzOTljNzBlYWEwOSIsInN1YiI6IjU5ZTlhNTg4OTI1MTQxMGM3NDAwYjUwMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._U1Aw268w9cBgvwMy5LGQr4qBBSLOwTgNPdwt59ZI6k",
+    baseUrl: "https://api.themoviedb.org/3",
+  },
 };
